0b72d8711552200cbc2f021d99c9bf033b1b9055
Raja Pitchai <raja.pitchai@grootan.com>
2021-03-11 13:08:29 +0530

    automation build

--- FlickrPhotoSearch/AppDelegate.swift
+++ FlickrPhotoSearch/AppDelegate.swift
@@ -31,6 +31,8 @@ Except as contained in this notice, the name of Raja Pitchai shall not
 be used in advertising or otherwise to promote the sale, use or other deal-
 ings in this Software without prior written authorization from him.
  
+ Automation 
+ 
 */
 
 import UIKit
